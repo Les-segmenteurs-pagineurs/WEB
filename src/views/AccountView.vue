00c260bfc8f5bdeacefe0df63038eec6cd61c115
Louis Schlegel <louisschlegel@wifi-eduroam-interne-172-29-79-95.wifi.unistra.fr>
2023-12-07 23:33:07 +0100

    MAJ account

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,34 +1,66 @@
 <template>
-    <div class="about">
-      <h1>Mon Compte</h1>
-      <form @submit.prevent="add_categorie">
-        <input type="text" v-model="login" @change="console.log(login)">
-        <input type="password" v-model="password" @change="console.log(password)">
-        <input type="submit" value="Ajouter">
-      </form>
+  <div id="user_and_history">
+    <div id="user_name"> 
+      <h1>Profil de l'utilisateur connecté</h1>
+      <div v-if="user">
+        <p>Nom d'utilisateur : {{ user.username }}</p>
+        <!-- Autres détails de l'utilisateur selon votre modèle -->
+        <div id="change_password">
+          
+        </div>
+      </div>
+      <div v-else>
+        <p>Aucun utilisateur connecté.</p>
+      </div>
     </div>
-  <div class="result_quizz">
-    <h1>Résultats Quizz</h1>
-    <!-- <div class="cats">
-        <Categorie v-for="categorie in categories" :key="categorie.id" :infos_categorie="categorie"/>
-    </div> -->
+
+    <div id="history_quizz"> <!-- Correction du nom de l'ID ici -->
+      <p>hey</p>
+    </div>
   </div>
-  </template>
-  
-  <script>  
-  export default {
-    name: 'AccountView',
-    
-  };
-  </script>
-  
-  <style>
-  /* @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  } */
-  </style>
-  
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+export default {
+  setup() {
+    const user = ref(null);
+
+    onMounted(async () => {
+      try {
+        // Faites un appel à l'API pour récupérer les détails de l'utilisateur connecté
+        const response = await axios.get('http://klebert-host.com:60513//get_login'); // Remplacez par l'URL correcte
+        user.value = response.data;
+      } catch (error) {
+        console.error('Erreur lors de la récupération de l\'utilisateur connecté', error);
+      }
+    });
+
+    return {
+      user,
+    };
+  },
+};
+</script>
+
+<style>
+  #user_and_history {
+    display: flex;
+    flex-direction: row;
+    gap: 20px; /* Espace entre les deux sections */
+  }
+
+  #user_name {
+    align-items: center;
+    background: #EDF5E1;
+    padding: 10px;
+  }
+
+  #history_quizz {
+    align-items: flex-start;
+    background: beige;
+    padding: 20px;
+  }
+</style>
