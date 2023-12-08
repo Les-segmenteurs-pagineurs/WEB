b44b010941c029cdc38c60aa68e48b930f61ea0c
Louis Schlegel <louisschlegel@wifi-eduroam-interne-172-29-79-95.wifi.unistra.fr>
2023-12-08 02:27:52 +0100

    MAJ

---
+++ src/compenent/AttemptHistoryLine.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="cat-wrapper">
+      <ul>
+        <li>login : {{ infos_categorie.login }}</li>
+        <li>nationalite : {{ infos_categorie.nationalite }}</li>
+      </ul>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AttemptHistoryLine',
+    props : {
+        infos_user : Object
+    },
+}
+</script>
+  
+<style>
+.cat-wrapper {
+    background-color: green;
+    border : 1px solid white;
+}
+@media (min-width: 1024px) {
+    .cat-wrapper {
+      display: flex;
+      align-items: center;
+    }
+}
+</style>
+  
--- src/views/AddAccountView.vue
+++ src/views/AddAccountView.vue
@@ -1,85 +1,93 @@
 <template>
-    <div id="add_account">
-        <h1>Ajouter un User</h1>
-        <form @submit.prevent="add_user">
-         <input type="text" v-model="login">
-         <input type="password" v-model="login">
-         <input type="password2" v-model="login">
-        <input type="submit" value="Ajouter" >
+  <div id="add_account">
+    <h1>Ajouter un Utilisateur</h1>
+    <form @submit.prevent="addUser">
+      <label>Login:</label>
+      <input type="text" v-model="login" />
+
+      <label>Mot de passe:</label>
+      <input type="password" v-model="password" />
+
+      <label>Confirmer le mot de passe:</label>
+      <input type="password" v-model="password2" />
+
+      <input type="submit" value="Ajouter" />
     </form>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  import axios from 'axios';
-  
-  export default {
-    setup() {
-      const user = ref(null);
-      add_user(async () => {
-        try {
-          // Faites un appel à l'API pour récupérer les détails de l'utilisateur connecté
-          const response = await axios.post('http://klebert-host.com:60513//get_login'); // Remplacez par l'URL correcte
-          user.value = response.data;
-        } catch (error) {
-          console.error('Erreur lors de la récupération de l\'utilisateur connecté', error);
-        }
-      });
-  
-      return {
-        user,
-        listeScores
-      };
-    },
-  };
-  </script>
-  
-  <style>
-  #user_and_history {
-    display: flex;
-    flex-direction: row;
-    gap: 20px; /* Espace entre les deux sections */
-  }
-  
-  #user_name {
-    align-items: center;
-    background: #bz; /* Bleu */
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  
-  .user-info {
-    font-size: 1.2em;
-    font-weight: bold;
-    color: #EDF5E1;
-  }
-  
-  #history_quizz {
-    align-items: flex-start;
-    background: #EDF5E1; /* Gris clair */
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  
-  .score-item {
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    margin-bottom: 10px;
-    padding: 15px;
-  }
-  
-  .score-date {
-    font-weight: bold;
-    color: #2c3e50; /* Gris foncé */
-  }
-  
-  .score-value {
-    color: #e74c3c; /* Rouge */
-    font-size: 1.1em;
-    margin-top: 5px;
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+
+export default {
+  setup() {
+    const login = ref('');
+    const password = ref('');
+    const password2 = ref('');
+
+    const addUser = async () => {
+        // Faites un appel à l'API pour enregistrer l'utilisateur
+        const response = await axios.post('http://klebert-host.com:60513/user/register', {
+          "login": login.value,
+          "password": password.value,
+          "password2": password2.value,
+        });
+        console.log('Utilisateur ajouté :', response.status);
+
+    };
+
+    return {
+      login,
+      password,
+      password2,
+      addUser,
+    };
+  },
+};
+</script>
+<style scoped>
+#add_account {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  font-size: 24px;
+  text-align: center;
+  color: #333;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+}
+
+label {
+  margin-bottom: 8px;
+  color: #555;
+}
+
+input {
+  padding: 8px;
+  margin-bottom: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #45a049;
+}
+
+</style>
