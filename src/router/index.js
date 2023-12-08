37d447af6f2028ae86c1a8ea3163b933d339abc6
Louis Schlegel <louisschlegel@wifi-eduroam-interne-172-29-79-95.wifi.unistra.fr>
2023-12-08 01:11:47 +0100

    MAJ index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AccountViewVue from '@/views/AccountView.vue'
+import AddAccount from '@/views/AddAccountView.vue'
+import DelAccount from '@/views/DelAccountView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +15,16 @@ const router = createRouter({
       path: '/user/account',
       name: 'account',
       component: AccountViewVue
+    },
+    {
+      path: '/user/add_account',
+      name: 'add_account',
+      component: AddAccount
+    },
+    {
+      path: '/user/del_account',
+      name: 'del_account',
+      component: DelAccount
     }
   ]
 })
